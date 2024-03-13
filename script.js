9b8e8717f55861dbe8609bb700a07d2dbf154d69
Umar Arif <umararif123987@gmail.com>
2024-03-13 12:59:42 +0500

    new

--- script.js
+++ script.js
@@ -8,3 +8,22 @@ function menutoggle() {
         MenuItems.style.maxHeight = "0px";
     }
 }
+
+
+
+const productContainers = [...document.querySelectorAll('.product-container')];
+const nxtBtn = [...document.querySelectorAll('.nxt-btn')];
+const preBtn = [...document.querySelectorAll('.pre-btn')];
+
+productContainers.forEach((item, i) => {
+    let containerDimensions = item.getBoundingClientRect();
+    let containerWidth = containerDimensions.width;
+
+    nxtBtn[i].addEventListener('click', () => {
+        item.scrollLeft += containerWidth;
+    })
+
+    preBtn[i].addEventListener('click', () => {
+        item.scrollLeft -= containerWidth;
+    })
+})
